2f4786d0e065651e5c130124fbf102e3d0520ac6
huang.peng <huang.peng@immomo.com>
2022-06-22 17:06:25 +0800

    feat: 🎸 完成泛型层序遍历

--- src/midium/61.InorderTraversal.ts
+++ src/midium/61.InorderTraversal.ts
@@ -1,6 +1,14 @@
 const tree1 = {
   val: 1,
-  left: null,
+  left: {
+    val: 22,
+    left: {
+      val: 44,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
   right: {
     val: 2,
     left: {
@@ -12,7 +20,26 @@ const tree1 = {
   },
 } as const;
 
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
 
+type InorderTraversal<T extends TreeNode | null> = T extends TreeNode
+  ? T["left"] extends TreeNode
+    ? T["right"] extends TreeNode
+      ? [
+          ...InorderTraversal<T["left"]>,
+          T["val"],
+          ...InorderTraversal<T["right"]>
+        ]
+      : [...InorderTraversal<T["left"]>, T["val"]]
+    : T["right"] extends TreeNode
+    ? [T["val"], ...InorderTraversal<T["right"]>]
+    : [T["val"]]
+  : [];
+// 中序遍历
 type A = InorderTraversal<typeof tree1>; // [1, 3, 2]
 
 export {};
